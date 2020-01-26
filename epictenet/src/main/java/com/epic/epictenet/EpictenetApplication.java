04a304792b83cd01812bafdc48465de179a0ba25
Hp <Hp@Naveen>
2020-01-26 23:34:11 +0530

    Swagger Docket Added

--- epictenet/src/main/java/com/epic/epictenet/EpictenetApplication.java
+++ epictenet/src/main/java/com/epic/epictenet/EpictenetApplication.java
@@ -3,7 +3,13 @@ package com.epic.epictenet;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -14,6 +20,26 @@ public class EpictenetApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpictenetApplication.class, args);
+	}
+	
+	@Bean
+	public Docket swaggerConfigeration(){
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				//.paths(PathSelectors.ant("/*"))
+				.apis(RequestHandlerSelectors.basePackage("com.epic.epictenet.controller"))
+				.build()
+				.apiInfo(apidetails());
+		
+		
+	}
+
+
+	private ApiInfo apidetails() {
+		return new ApiInfo(
+				"Naveen API Doc", "Api to create Post and add comment" ,"1.0" ,"", "",  "Naveen Prabhakar", "8105181845"
+				);
 	}
 
 	
